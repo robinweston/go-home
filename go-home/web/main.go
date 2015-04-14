bc1123f7563c01e4ee7fa81bbf3bafab85c44213
Robin Weston <lcfcforever@hotmail.com>
2015-04-14 16:03:35 +0100

    Display time till hometime

--- go-home/web/main.go
+++ go-home/web/main.go
@@ -8,7 +8,20 @@ import (
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	now := time.Now().Local()
-	io.WriteString(w, "Hello world! The current local time is " + now.String())
+	day := now.Weekday()
+
+	var message = ""
+	if day == time.Saturday || day == time.Sunday {
+		message = "It's the weekend. Let's forget about work!"
+	} else if now.Hour() < 9 || now.Hour() > 18 {
+		message = "You really shouldn't be at work now"
+	} else {
+		endOfDay := time.Date(now.Year(), now.Month(), now.Day(), 18, 0, 0, 0, time.Local)
+		timeTillHome := endOfDay.Sub(now)
+		message = "You get to go home in " + timeTillHome.String()
+	}
+
+	io.WriteString(w, "The current local time is "+now.String()+".\n"+message)
 }
 
 func main() {
